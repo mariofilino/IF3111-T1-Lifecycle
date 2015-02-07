0e302ff16f3da1facda47ba6a72881831f99706e
MarioFilino <mario.filino@gmail.com>
2015-02-07 21:08:49 +0700

    tutorial 2 - 1. Explicit Intent

--- ExplicitIntent/app/src/main/java/com/android/mario/explicit/intent/MainActivity.java
+++ ExplicitIntent/app/src/main/java/com/android/mario/explicit/intent/MainActivity.java
@@ -5,8 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private static final int REQUEST_CODE = 10;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,19 @@ public class MainActivity extends Activity {
         intent.putExtra("value1", value);
 
         // TODO 2 start second activity with
-        startActivity(intent);
+        startActivityForResult(intent, REQUEST_CODE);
     }
+    
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            if (data.hasExtra("value2")) {
+                String result = data.getExtras().getString("value2");
+                if (result != null && result.length() > 0) {
+                    Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
 }
--- ExplicitIntent/app/src/main/java/com/android/mario/explicit/intent/ResultActivity.java
+++ ExplicitIntent/app/src/main/java/com/android/mario/explicit/intent/ResultActivity.java
@@ -5,8 +5,10 @@ package com.android.mario.explicit.intent;
  */
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.EditText;
 
 public class ResultActivity extends Activity {
 
@@ -20,5 +22,22 @@ public class ResultActivity extends Activity {
         String value1 = extras.getString("value1");
         TextView t = (TextView) findViewById(R.id.displayintentextra);
         t.setText(value1);
+    }
+
+    @Override
+    public void finish() {
+
+        // Create new intent
+        Intent intent = new Intent();
+
+        // Read the data of the EditText field
+        EditText text = (EditText) findViewById(R.id.returnValue);
+
+        // Put the text from EditText
+        intent.putExtra("value2", text.getText().toString());
+
+        setResult(RESULT_OK, intent);
+
+        super.finish();
     }
 }
