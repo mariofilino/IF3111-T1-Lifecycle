93fdb40819b7e98dc0ada13fee190fa2c8de9f5e
MarioFilino <mario.filino@gmail.com>
2015-02-08 09:02:40 +0700

    tutorial 3 - 2. Alarm

---
+++ Alarm/app/src/main/java/com/android/mario/alarm/MyBroadcastReceiver.java
@@ -0,0 +1,23 @@
+package com.android.mario.alarm;
+
+/**
+ * Created by Sakurai on 2/8/2015.
+ */
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Nananana....",
+                Toast.LENGTH_LONG).show();
+        // Vibrate the mobile phone
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(2000);
+    }
+
+}
